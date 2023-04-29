2732ce6c540f4ea137b2927773587c85fc641138
jasper-nguyen <118552965+jasper-nguyen@users.noreply.github.com>
2023-04-28 22:11:20 -0700

    Update bug1.py

--- bug1.py
+++ bug1.py
@@ -1,20 +1,21 @@
 # TODO: there's code missing in one or more lines below
 class Base:
     def __init__(self, x, y, size):
+        # TODO: will need to fill this in
+        #Initialize x, y and size variable from parameters
         self.x = x
         self.y = y
         self.size = size
-
+        
     def draw(self):
         return ""
-
-
-class Circle(Base):
+        
+class Circle(Base): #Add parameter to circle class and change init so that circle inhertets from base class
     pass
-
+        
     def draw(self):
         return f"""
-
+        
 ({self.x}, {self.y})
 {self.size}
          , - ~ ~ ~ - ,
@@ -29,12 +30,10 @@ class Circle(Base):
      ,                  , '
        ' - , _ _ _ ,  '
               """
-
-
 class Square(Base):
-    pass
-
-    def draw(self):
+    pass #Change init to pass so that square class inherets from base parent class
+        
+    def draw(self): #Add self so that draw can return the shape
         return f"""
 ({self.x}, {self.y})
 {self.size}
@@ -50,14 +49,14 @@ class Square(Base):
 --------------------
 """
 
+# All of the code below is correct
 def draw_any_shape(myShape):
     print(myShape.draw())
-
-
+    
 def main():
-    s = Square(1, 2, 3)
+    s = Square(1,2,3)
     draw_any_shape(s)
-
-    c = Circle(2, 2, 1)
+    
+    c = Circle(2,2,1)
     draw_any_shape(c)
 main()
